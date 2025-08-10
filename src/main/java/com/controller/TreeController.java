e861e159aab1ec29f34d2c960b5f4475c454efa8
Noah-Hickey <Noah.Hickey@keyin.com>
2025-08-10 15:32:30 -0230

    Tree Node class and TreesApplication file completed.

---
+++ src/main/java/com/controller/TreeController.java
@@ -0,0 +1,14 @@
+public class TreeController {
+
+    public void createTree(String treeName) {
+
+    }
+
+    public void deleteTree(String treeId) {
+
+    }
+
+    public void getTree(String treeId) {
+
+    }
+}
