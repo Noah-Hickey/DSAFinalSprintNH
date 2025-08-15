1c9abb7f25d7c769a7c283c71b552cef75afba68
Noah-Hickey <Noah.Hickey@keyin.com>
2025-08-15 20:56:01 -0230

    Added html files, updated application.properties, added TreeServiceTests file, updated some of the tree files.

--- src/main/java/com/repository/TreesApplication.java
+++ src/main/java/com/repository/TreesApplication.java
@@ -1,13 +1,16 @@
-package com.trees;
+package com.repository;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EntityScan(basePackages = "com.model")
+@EnableJpaRepositories(basePackages = "com.repository")
 @SpringBootApplication
 public class TreesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TreesApplication.class, args);
 	}
-
 }
